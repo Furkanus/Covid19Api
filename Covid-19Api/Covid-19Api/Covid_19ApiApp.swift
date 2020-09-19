af3013426e5244d103d5c775a0f159b7bbd4aec8
Furkan Hanci <60580341+Furkanus@users.noreply.github.com>
2020-09-19 21:03:38 +0300

    Add files via upload

---
+++ Covid-19Api/Covid-19Api/Covid_19ApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Covid_19ApiApp.swift
+//  Covid-19Api
+//
+//  Created by Furkan Hanci on 9/19/20.
+//
+
+import SwiftUI
+
+@main
+struct Covid_19ApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
